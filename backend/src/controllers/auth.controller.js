3867d7059e2653c9a5e75731b3507eeab097b30a
jaskirat-singh-12 <jaskirat7572@gmail.com>
2025-08-16 01:29:55 +0530

    Enhance cookie security by adding httpOnly, secure, and sameSite attributes for token cookies in authentication controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -20,7 +20,11 @@ async function registerController(req, res) {
   });
   
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,    // JS on frontend can't read it
+  secure: true,      // send only over HTTPS
+  sameSite: "None",
+  });
 
   res.status(201).json({
     message: "User registered successfully",
@@ -43,7 +47,11 @@ async function loginController(req, res) {
   }
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res.cookie("token", token);
+  res.cookie("token", token,{
+    httpOnly: true,    // JS on frontend can't read it
+  secure: true,      // send only over HTTPS
+  sameSite: "None",
+  });
 
   res.status(201).json({
     message: "User logged in successfully",
